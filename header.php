8d9e522fd7e09c8991669bcd662d15f59e69b314
Roland HUON <huonroland91@gmail.com>
2023-11-04 22:37:45 +0100

    Responsive header finish

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'refontepingpassion' ); ?></button>
+			<button class="menu-toggle" aria-controls="header-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'refontepingpassion' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
